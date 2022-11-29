5ace9d94083834e447274ca4ea35974255524dbf
Miguel <mirkouda@gmail.com>
2022-11-29 13:36:04 +0100

    Components created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Profile from './components/Profile/Profile';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/home' element={<Home />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/register' element={<Register />}/>
+          <Route path='/profile' element={<Profile />}/>
+      </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
