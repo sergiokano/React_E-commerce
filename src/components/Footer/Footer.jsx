efaa212bfc187a8b57388b0ee7b1d019b9e40326
sergiokano <sergiocano.design@gmail.com>
2022-11-29 14:01:39 +0100

    Footer done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,23 +1,66 @@
 import React from "react";
 import "./Footer.scss";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+import paymentImg from "../img/payment.png";
 
 function Footer() {
   return (
-    <div className="main-footer">
-      <div className="container">
-        <p className="col-sm">
-          &copy;{new Date().getFullYear()}
-          <a href="http://www.linkedin.com/in/sergiocano-dev">
-            <span> designed by theKano-dev_xXX44Xx && </span>
-          </a>
-          <a href="https://www.linkedin.com/in/mherrevi/">
-            <span> Miguel Herrera </span>
-          </a>
-          <Link to="/"> Home </Link> | <Link to="/news"> News </Link> |{" "}
-          <Link to="/create"> Post an Article </Link> | All rights reserved |
-          Terms Of Service | Privacy |
-        </p>
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit
+            amet conse ctetur adipisicing elit, seddo eiusmod tempor incididunt
+            ut labore etdolore.
+          </span>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit
+            amet conse ctetur adipisicing elit, seddo eiusmod tempor incididunt
+            ut labore etdolore.
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">Cano'smetics</span>
+          <span className="copyright">
+            <span>© Copyright {new Date().getFullYear()}</span>
+            <span> designed by </span>
+            <a href="http://www.linkedin.com/in/sergiocano-dev">
+              <span>theKano-dev_xXX44Xx</span>
+            </a>
+            <span> && </span>
+            <a href="https://www.linkedin.com/in/mherrevi/">
+              <span> Miguel Herrera </span>
+            </a>
+            <span> All Rights Reserved</span>
+          </span>
+        </div>
+        <div className="right">
+          <img src={paymentImg} alt="" />
+        </div>
       </div>
     </div>
   );
