147270d5ea3cff48e1604bfe4a12ded1a84e0cf0
sergiokano <sergiocano.design@gmail.com>
2022-12-08 18:18:45 +0100

    home route fixed

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
             <OrderProvider>
               <Header />
               <Routes>
-                <Route path="/home" element={<Home />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/products" element={<Products />} />
---
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./FeaturedProducts.scss"
+
+const FeaturedProducts = () => {
+  return (
+    <div className='FeaturedProducts'>FeaturedProducts</div>
+  )
+}
+
+export default FeaturedProducts
