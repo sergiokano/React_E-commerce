ab99406198a97de77f8d502aed80466cf7d1a6f7
Miguel <mirkouda@gmail.com>
2022-12-10 20:17:41 +0100

    Image added to Profile-Order

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../context/UserContext/UserState";
 import { Spin, Card } from 'antd';
 import { PhoneOutlined, MailOutlined, TagOutlined } from "@ant-design/icons";
@@ -6,6 +6,10 @@ import "./Profile.scss";
 
 const Profile = () => {
   const { user, getUserInfo } = useContext(UserContext);
+  const [selectedImage] = useState (0);
+  const images = [
+    "https://cdn.shopify.com/s/files/1/0574/0127/8625/products/Wildsmith_EyeSerum_HR_bae48101-5214-4b44-97aa-6b4570fc666b_1000x.png?v=1623667289",
+  ];
   useEffect(() => {
     getUserInfo();
   }, []);
@@ -32,6 +36,7 @@ const Profile = () => {
             <p><TagOutlined /> {user.address}</p>
             <br />
             <span>{user?.Orders?.map(order => order.Products.map(product => <p key={product.id}> Your order: {product.name}</p>))}</span>
+            <img src={images[selectedImage]} alt="" width={250}/>
 
           </Card>
         </div>
