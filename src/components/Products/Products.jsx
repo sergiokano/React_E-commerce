2e2e946bf36909ca90de7f24f6090931637f2f20
Miguel <mirkouda@gmail.com>
2022-12-10 20:06:36 +0100

    Image added to Products card

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ProductsContext } from "../../context/ProductsContext/ProductsState";
 import { Card, Button } from "antd";
 import "./Products.scss";
@@ -7,8 +7,14 @@ import { Link } from "react-router-dom";
 import "./Products.scss"
 
 
+
 const Products = () => {
   const { products, getProducts, addCart, cart } = useContext(ProductsContext);
+  const [selectedImage, setSelectedImage] = useState(0);
+  const images = [
+    "https://cdn.shopify.com/s/files/1/0574/0127/8625/products/Wildsmith_EyeSerum_HR_bae48101-5214-4b44-97aa-6b4570fc666b_1000x.png?v=1623667289",
+    "https://cdn.shopify.com/s/files/1/0574/0127/8625/products/Wildsmith_EyeSerum_Outer_HR_1000x.png?v=1623667289",
+  ];
 
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(cart));
@@ -24,34 +30,35 @@ const Products = () => {
             <div key={product.id} className="site-card-border-less-wrapper z-index">
               <br />
               <div className="card-sass">
-              <Card 
-                title={product.name}
-                bordered={true}
-                style={{
-                  width: 300,
-                  border: "1px solid gray",
-                  background: "transparent",
-                  color: "lightGray",
-                }} 
-              >
-                <p>{product.price}</p>
-                <Button style={{
-                  size: "small",
-                  border: "1px solid gray",
-                  background: "transparent",
-                  color: "lightGray",
-                }}
-                  onClick={() => addCart(product)}>
-                  <ShoppingOutlined />
-                </Button>
-                <Button style={{
-                  size: "small",
-                  border: "1px solid gray",
-                  background: "transparent",
-                  color: "lightGray",
-                  margin: "10px"
-                }}><Link to={'product/' + product.id}>See product</Link></Button>
-              </Card>
+                <Card
+                  title={product.name}
+                  bordered={true}
+                  style={{
+                    width: 300,
+                    border: "1px solid gray",
+                    background: "transparent",
+                    color: "lightGray",
+                  }}
+                >
+                  <img src={images[selectedImage]} alt="" width={200}/>
+                  <p>{product.price}</p>
+                  <Button style={{
+                    size: "small",
+                    border: "1px solid gray",
+                    background: "transparent",
+                    color: "lightGray",
+                  }}
+                    onClick={() => addCart(product)}>
+                    <ShoppingOutlined />
+                  </Button>
+                  <Button style={{
+                    size: "small",
+                    border: "1px solid gray",
+                    background: "transparent",
+                    color: "lightGray",
+                    margin: "10px"
+                  }}><Link to={'product/' + product.id}>See product</Link></Button>
+                </Card>
               </div>
             </div>
           </>);
