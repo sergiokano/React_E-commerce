f38af1d69d053836f75a1b6120d79677b6a41168
Miguel <mirkouda@gmail.com>
2022-12-12 19:02:01 +0100

    About text changed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -13,16 +13,29 @@ const About = () => {
                 </div>
             </div>
             <div className="right">
-                <p>La tienda online de cosmética natural donde comprar todos los productos de Freshly Cosmetics.
-                    Siente en tu piel los mejores productos con +99% ingredientes naturales de calidad con resultados reales.
-                    <br />
-                    <br />
-                    Porque creemos que se pueden hacer las cosas de otra forma, formulamos cosmética vegana, segura y sostenible que generará un gran cambio en tu piel y en el planeta.
-                    Te enamorarán las texturas y aromas de nuestros productos que además, son aptos para pieles sensibles. Tus productos Freshly llegarán en un paquete 100% reciclable plastic-free, con envases fabricados en vidrio, aluminio, cartón y madera, ¡totalmente reutilizables y reciclables!
-                    <br />
-                    <br />
-                    Cuida tu piel con la mejor cosmética natural de Freshly Cosmetics.</p>
+                <p>WE WORK WITH THE LEADING COSMETIC CLINICAL TRIAL SPECIALISTS TO CONDUCT TARGETED INDEPENDENT TRIALS THAT TEST THE EFFICACY OF OUR FORMULATIONS. WE ARE ALSO PROUD TO PARTNER WITH BEAUTY BIBLE, THE UK’S DEFINITIVE SOURCE OF GLOBAL BEAUTY PRODUCT REVIEWS. THEY CONDUCT INDEPENDENT BLIND TRIALS OF OUR ENTIRE RANGE WITH A PANEL OF 50 DISCERNING BEAUTY-INVOLVED CONSUMERS.</p>
                 <br />
+                <p>COMPOSTABLE PACKAGING
+                    Mycelium is the vegetative root structure of a mushroom which is grown around a mould and forms a custom-sized, secure structure. To make the product stronger, locally sourced agricultural waste such as corn and barley husks are added to the mycelium. The final packaging quite literally grows around its mould in a matter of days.
+                    <br />
+                    <br />
+                    1. It begins with AGRICULTURAL waste
+                    <br />
+                    Agricultural waste, such as corn and barley husks are collected from farmers, then sorted and cleaned. The mixture is ground before the start of the Mycelium growing process.
+                    <br /> <br />
+                    2. Adding MYCELIUM
+                    <br />
+                    The root system of mushrooms, mycelium, is added to the mix of agricultural waste. It acts as nature’s glue to bind it together around a mould.
+                    <br />
+                    <br />
+                    3. MUSHROOM material
+                    <br />
+                    After a few days of growth it is placed in a heat press to remove any excess moisture and prevent further mycelium growth. When dry the material can be removed from the mould and is ready to use.
+                    <br />
+                    <br />
+                    4. COMPOSTING
+                    <br />
+                    The material is fully biodegradable, but won’t start to degrade unless it is exposed to the right conditions. This packaging can be composted and the mycelium will break down in about a month. As soon as it is exposed to regular moisture it will disappear withouta trace.</p>
             </div>
         </div>
     )
